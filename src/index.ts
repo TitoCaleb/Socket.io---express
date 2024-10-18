0e8c5b533d0f66540a1f5f71e5eff2f6625644ba
TitoCaleb <calebcastro2604@gmail.com>
2024-10-18 12:39:56 -0500

    feat: broadcast

--- src/index.ts
+++ src/index.ts
@@ -9,47 +9,10 @@ const io = new Server(httpServer);
 
 app.use(express.static(path.join(__dirname, "views")));
 
-const socketOnline: string[] = [];
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-io.on("connection", (socket) => {
-  socketOnline.push(socket.id);
-
-  console.log("Clientes conectados", io.engine.clientsCount);
-  console.log("Id del socket conectado", socket.id);
-  socket.emit("welcome_message", "Ahora estás conectado al servidor 😎.");
-
-  socket.on("client_message", (data) => {
-    console.log(data);
-  });
-
-  // Emisión a todos los clientes
-  io.emit("everyone", `El client con el id ${socket.id} se ha conectado`);
-
-  // Emisión a uno solo
-  socket.on("message_to_last", (data) => {
-    const lastSocket = socketOnline.at(-1);
-
-    if (!lastSocket) {
-      return;
-    }
-
-    io.to(lastSocket).emit("send_message_to_last", data);
-  });
-
-  //on, once, off
-  socket.emit("on", "Buenas");
-  socket.emit("on", "Buenas");
-
-  socket.emit("once", "Buenas 2 veces");
-  socket.emit("once", "Buenas 2 veces");
-
-  socket.on("disconnect", () => {
-    console.log(`El socket ${socket.id} se desconecto`);
-  });
-});
+io.on("connection", (socket) => {});
 
 httpServer.listen(3000);
