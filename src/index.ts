4a0aa6d6a9a3cb016f1910f667571d2b7872e168
TitoCaleb <calebcastro2604@gmail.com>
2024-10-18 14:49:14 -0500

    feat: uso de las salas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { createServer } from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import path from "path";
 
 const app = express();
@@ -13,6 +13,44 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-io.on("connection", (socket) => {});
+interface SocketPersonalized extends Socket {
+  connectedRoom: string;
+}
+
+io.on("connection", (socket: Socket) => {
+  const personalizedSocket = socket as SocketPersonalized;
+  personalizedSocket.connectedRoom = "";
+
+  socket.on("connect_to_room", ({ room }) => {
+    socket.leave(personalizedSocket.connectedRoom);
+
+    switch (room) {
+      case "room1":
+        socket.join("room1");
+        personalizedSocket.connectedRoom = "room1";
+        break;
+      case "room2":
+        socket.join("room2");
+        personalizedSocket.connectedRoom = "room2";
+
+        break;
+      case "room3":
+        socket.join("room3");
+        personalizedSocket.connectedRoom = "room3";
+        break;
+      default:
+        break;
+    }
+  });
+
+  socket.on("message", (message) => {
+    const room = personalizedSocket.connectedRoom;
+
+    io.to(room).emit("send_message", {
+      message,
+      room,
+    });
+  });
+});
 
 httpServer.listen(3000);
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,1 +1,36 @@
 const socket = io();
+
+//Botones
+const connectRoom1 = document.getElementById("connectRoom1");
+const connectRoom2 = document.getElementById("connectRoom2");
+const connectRoom3 = document.getElementById("connectRoom3");
+
+connectRoom1.addEventListener("click", () => {
+  socket.emit("connect_to_room", { room: "room1" });
+});
+connectRoom2.addEventListener("click", () => {
+  socket.emit("connect_to_room", { room: "room2" });
+});
+connectRoom3.addEventListener("click", () => {
+  socket.emit("connect_to_room", { room: "room3" });
+});
+
+// Botón mensaje
+const sendMessage = document.getElementById("sendMessage");
+
+// Enviar mensaje
+sendMessage.addEventListener("click", () => {
+  const message = prompt("Escribe tu mensaje");
+
+  socket.emit("message", message);
+});
+
+// Recibir mensaje
+socket.on("send_message", ({ room, message }) => {
+  const li = document.createElement("li");
+  li.innerText = message;
+
+  console.log(li);
+
+  document.querySelector(`#${room}`).appendChild(li);
+});
