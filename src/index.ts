4194a156e2a650fa9f9b7d06e58199b3a5712e00
TitoCaleb <calebcastro2604@gmail.com>
2024-10-20 01:57:18 -0500

    feat: eventos volatiles evitar el buffering

--- src/index.ts
+++ src/index.ts
@@ -16,39 +16,8 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  socketOnline.push(socket.id);
-
-  console.log("Clientes conectados", io.engine.clientsCount);
-  console.log("Id del socket conectado", socket.id);
-  socket.emit("welcome_message", "Ahora estás conectado al servidor 😎.");
-
-  socket.on("client_message", (data) => {
-    console.log(data);
-  });
-
-  // Emisión a todos los clientes
-  io.emit("everyone", `El client con el id ${socket.id} se ha conectado`);
-
-  // Emisión a uno solo
-  socket.on("message_to_last", (data) => {
-    const lastSocket = socketOnline.at(-1);
-
-    if (!lastSocket) {
-      return;
-    }
-
-    io.to(lastSocket).emit("send_message_to_last", data);
-  });
-
-  //on, once, off
-  socket.emit("on", "Buenas");
-  socket.emit("on", "Buenas");
-
-  socket.emit("once", "Buenas 2 veces");
-  socket.emit("once", "Buenas 2 veces");
-
-  socket.on("disconnect", () => {
-    console.log(`El socket ${socket.id} se desconecto`);
+  socket.on("circle_position", (position) => {
+    socket.broadcast.emit("move_circle", position);
   });
 });
 
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,53 +1,32 @@
 const socket = io();
 
-socket.on("welcome_message", (data) => {
-  document.getElementById("text").innerText = data;
+const circle = document.getElementById("circle");
+
+const drawCircle = (position) => {
+  circle.style.top = position.top;
+  circle.style.left = position.left;
+};
+
+const drag = (e) => {
+  const position = {
+    top: e.clientY + "px",
+    left: e.clientX + "px",
+  };
+
+  drawCircle(position);
+  console.log("Se envía el evento al servidor");
+  socket.volatile.emit("circle_position", position);
+  // Añadir volatile para que no se envíe el evento a todos los clientes
+};
+
+document.addEventListener("mousedown", (e) => {
+  document.addEventListener("mousemove", drag);
 });
 
-const emitToServer = document.querySelector("#emit-to-server");
-emitToServer.addEventListener("click", () => {
-  socket.emit("client_message", "¡Hola, servidor! 😎");
+document.addEventListener("mouseup", (e) => {
+  document.removeEventListener("mousemove", drag);
 });
 
-const emitToLast = document.querySelector("#emit-to-last");
-emitToLast.addEventListener("click", () => {
-  socket.emit("message_to_last", "¡Hola, has entrado al ultimo! 😎");
+socket.on("move_circle", (position) => {
+  drawCircle(position);
 });
-
-socket.on("send_message_to_last", (data) => {
-  console.log(data);
-});
-
-socket.on("everyone", (data) => {
-  console.log(data);
-});
-
-socket.on("on", () => {
-  console.log("[ON] Evento que se emite varias veces");
-});
-
-socket.once("once", () => {
-  console.log("[ONCE] Evento que se emite una sola vez");
-});
-
-//? Eventos de estado que se pueden escuchar
-// function checkSocketStatus() {
-//     console.log("Estado del socket:", socket.connected);
-//   }
-// socket.on("connect", () => {
-//   console.log("Connected to server", socket.id);
-//   checkSocketStatus();
-// });
-
-// socket.on("disconnect", () => {
-//   console.log("Disconnected from server", socket.id);
-//   checkSocketStatus();
-// });
-
-// socket.on("reconnect_attempt", (att) => {
-//   console.log("Intentando reconectar... 🙁");
-// });
-
-// socket.on("reconnect", (att) => {
-//   console.log("!Me he vuelto a conectar! 😌");
-// });
